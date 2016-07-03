b53a56a7512e6cc84427288b01f327646bf605d1
BoBRoID <n.gilko@gmail.com>
2016-07-03 12:38:56 +0300

    default admin

--- backend/modules/news/views/default/index.php
+++ backend/modules/news/views/default/index.php
@@ -43,7 +43,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         ]));
                     }
                 ],
-                'category',
+                'categoryID',
                 'author',
                 [
                     'attribute' =>  'publishDate',
---
+++ console/migrations/m160703_093322_default_admin.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+class m160703_093322_default_admin extends Migration
+{
+    public function up()
+    {
+        $admin = new \backend\models\Siteuser([
+            'username'  =>  'admin'
+        ]);
+
+        $admin->setPassword('defaultPassword');
+
+        $admin->save(false);
+    }
+
+    public function down()
+    {
+        echo "m160703_093322_default_admin cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
